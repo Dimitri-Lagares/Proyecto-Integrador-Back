fb40bdd6e6578a340723cfce97a5b63844635bb0
DragonSArmS <lagares.dimitri@gmail.com>
2023-04-27 08:19:24 -0500

    cors fixed

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 import { pool } from './db.js'
-import bodyParser from 'body-parser'
 import { PORT } from './config.js';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
