2d712058e18f82a62996c5f657cf597e6bb93b4e
Dimitri Lagares Gutiérrez <lagares.dimitri@gmail.com>
2023-05-02 20:12:29 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,11 +10,6 @@ const whiteList = [ 'http://localhost:5173' ]
 app.use(bodyParser.json());
 app.use(cors({origin: whiteList}));
 
-header('Access-Control-Allow-Origin *');
-header('Access-Control-Allow-Headers Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-header('Access-Control-Allow-Methods GET, POST, PUT, DELETE');
-header('Allow', 'GET, POST, PUT, DELETE');
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
